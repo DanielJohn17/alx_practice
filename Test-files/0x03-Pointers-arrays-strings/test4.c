401da1ede46127d0637bfe7a27aaedf56fc613b7
DanielJohn17 <danielyohannes38@gmail.com>
2023-07-02 22:35:12 +0300

    test files

---
+++ Test-files/0x03-Pointers-arrays-strings/test4.c
@@ -0,0 +1,24 @@
+#include "test.h"
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, bool;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		bool = 1;
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				bool = 0;
+				break;
+			}
+		}
+		if (bool)
+		{
+			break;
+		}
+	}
+	return (i);
+}
