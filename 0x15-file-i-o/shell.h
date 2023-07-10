918ba4665cca13df46ec754529761eb5a03eca8e
DanielJohn17 <danielyohannes38@gmail.com>
2023-07-10 21:56:27 +0300

    test file 1

--- 0x15-file-i-o/shell.h
+++ 0x15-file-i-o/shell.h
@@ -2,7 +2,12 @@
 #define _SHELL_H_
 
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int sub(int num1, int num2);
+ssize_t read_textfile(const char *filename, size_t letters);
 
 #endif
