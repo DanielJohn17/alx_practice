a6319101e3276822ee9b7e0aa73ba18c482704e5
DanielJohn17 <danielyohannes38@gmail.com>
2023-07-23 19:52:16 +0300

    excutable file

--- simple_shell/excute.c
+++ simple_shell/excute.c
@@ -3,13 +3,13 @@
 void exec(int argc, char **argument, char **av)
 {
 	pid_t pid;
-	char *command = argument[0];
+
 	if (argc > 0)
 	{
 		pid = fork();
 		if (pid == 0)
 		{
-			if (execve(command, argument, NULL) == -1)
+			if (execve(argument[0], argument, NULL) == -1)
 			{
 				perror(av[0]);
 				exit(EXIT_FAILURE);
