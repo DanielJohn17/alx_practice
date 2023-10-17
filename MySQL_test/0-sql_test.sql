d626cab33063fa766f7d90777180ec439dbd4d30
DanielJohn17 <danielyohannes38@gmail.com>
2023-10-17 23:06:17 +0300

    MySQL

---
+++ MySQL_test/0-sql_test.sql
@@ -0,0 +1,7 @@
+Select * From first_table;
+
+SHOW CREATE TABLE first_table;
+
+INSERT INTO first_table (id, name) VALUES (89, "Daniel Yohannes");
+
+SELECT COUNT(*) as id FROM first_table WHERE id=89;
---
+++ MySQL_test/README.md
@@ -0,0 +1,1 @@
+# Test for MySQL# Test for MySQL# Test for MySQL
