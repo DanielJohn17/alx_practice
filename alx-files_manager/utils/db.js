f539c6ff66288aed03a2d2be2b5032326e975353
DanielJohn17 <danielyohannes38@gmail.com>
2024-10-15 19:03:11 +0300

    DBClient class

---
+++ alx-files_manager/utils/db.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+import mongodb from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const dbURL = `mongodb://${host}:${port}/${database}`;
+
+    this.client = new mongodb.MongoClient(dbURL, { useUnifiedTopology: true });
+    this.client.connect();
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    return this.client.db().collection('user').countDocuments();
+  }
+
+  async nbFiles() {
+    return this.client.db().collection('files').countDocuments();
+  }
+}
+
+export const dbClient = new DBClient();
+export default dbClient;
